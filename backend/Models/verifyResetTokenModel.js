ded98775a6e9fdbcbc462a5df0669e810d2d6b53
unknown <lolyou331@yandex.com>
2023-07-21 23:25:42 +0200

    document & user models and controllers

--- backend/Models/verifyResetTokenModel.js
+++ backend/Models/verifyResetTokenModel.js
@@ -3,23 +3,23 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const verifyResetTokenSchema = new Schema({
-  _userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "User",
-  },
-  token: { type: String, required: true },
-  createdAt: {
-    type: Date,
-    required: true,
-    default: Date.now,
-    expires: 900,
-  },
+	_userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		required: true,
+		ref: "User",
+	},
+	token: { type: String, required: true },
+	createdAt: {
+		type: Date,
+		required: true,
+		default: Date.now,
+		expires: 900,
+	},
 });
 
 const VerifyResetToken = mongoose.model(
-  "VerifyResetToken",
-  verifyResetTokenSchema
+	"VerifyResetToken",
+	verifyResetTokenSchema
 );
 
 export default VerifyResetToken;
--- backend/helpers/emailTransport.js
+++ backend/helpers/emailTransport.js
@@ -5,18 +5,18 @@ import mg from "nodemailer-mailgun-transport";
 let transporter;
 
 if (process.env.NODE_ENV === "development") {
-  transporter = nodemailer.createTransport({
-    host: "mailhog",
-    port: 1025,
-  });
+	transporter = nodemailer.createTransport({
+		host: "mailhog",
+		port: 1025,
+	});
 } else if (process.env.NODE_ENV === "production") {
-  const mailgunAuth = {
-    auth: {
-      api_key: process.env.MAILGUN_API_KEY,
-      domain: process.env.MAILGUN_DOMAIN,
-    },
-  };
-  transporter = nodemailer.createTransport(mg(mailgunAuth));
+	const mailgunAuth = {
+		auth: {
+			api_key: process.env.MAILGUN_API_KEY,
+			domain: process.env.MAILGUN_DOMAIN,
+		},
+	};
+	transporter = nodemailer.createTransport(mg(mailgunAuth));
 }
 
 export default transporter;
